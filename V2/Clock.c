b7a16e97c703ae2a1229d14ad61dd32754fb7d7a
Diego <38155922+Diegomcha@users.noreply.github.com>
2024-03-19 00:51:49 +0100

    style(v2): :art: formatted Clock.c

--- V2/Clock.c
+++ V2/Clock.c
@@ -3,15 +3,16 @@
 #include "Processor.h"
 // #include "ComputerSystemBase.h"
 
-int tics=0;
+int tics = 0;
 
-void Clock_Update() {
+void Clock_Update()
+{
 	tics++;
-    // ComputerSystem_DebugMessage(NO_TIMED_MESSAGE,97,CLOCK,tics);
+	// ComputerSystem_DebugMessage(NO_TIMED_MESSAGE,97,CLOCK,tics);
 }
 
-
-int Clock_GetTime() {
+int Clock_GetTime()
+{
 
 	return tics;
 }
